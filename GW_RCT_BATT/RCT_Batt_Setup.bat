8482a6347018345f374ecdf1e7dcf1a8830c2d48
Geierwally <33550915+Geierwally-xD@users.noreply.github.com>
2018-01-25 05:53:10 +0000

    Update RCT_Batt_Setup.bat

--- GW_RCT_BATT/RCT_Batt_Setup.bat
+++ GW_RCT_BATT/RCT_Batt_Setup.bat
@@ -6,8 +6,8 @@
 @echo connect your transmitter USB and type in the drive letter e.g. E
 @Set /P transmitter=SET transmitter:
 @echo ================================
-IF not exist %transmitter%:\Model\*.* goto  FAILPATHset
-local
+IF not exist %transmitter%:\Model\*.* goto  FAILPATH
+setlocal
 %transmitter%:
 IF not exist %transmitter%:\apps\*.* md apps
 endlocal
