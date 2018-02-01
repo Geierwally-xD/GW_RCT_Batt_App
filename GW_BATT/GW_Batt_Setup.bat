6819f68b23938295212c50ac262c66ee61ecb0bd
Geierwally <33550915+Geierwally-xD@users.noreply.github.com>
2018-02-01 19:17:34 +0100

    new path structure, prepared for Jeti app manager readme.md changed

---
+++ GW_BATT/GW_Batt_Setup.bat
@@ -0,0 +1,81 @@
+@REM Installation of RCT-Batt App V2.34 on Jeti Transmitters
+@echo off
+@echo ================================
+@echo connection  transmitter
+@echo ================================
+@echo connect your transmitter USB and type in the drive letter e.g. E
+@Set /P transmitter=SET transmitter:
+@echo ================================
+IF not exist %transmitter%:\Model\*.* goto FAILPATH
+IF not exist %transmitter%:\apps\*.* goto FAILLUA
+@echo ================================
+@echo Partial transmitter backup required ?
+@echo ================================
+@echo If partial backup should be done, type in Y or J 
+@Set /P backup=SET backup:
+IF "%backup%"=="Y" goto PARTIAL
+IF "%backup%"=="y" goto PARTIAL
+IF "%backup%"=="J" goto PARTIAL
+IF "%backup%"=="j" goto PARTIAL
+goto INSTAL
+
+:PARTIAL
+@echo ================================
+@echo generating partial backup of transmitter, pleas wait
+@echo ================================
+set "backupFolder=%date%_partial"
+IF exist %backupFolder% rd %backupFolder% /S /Q
+md %backupFolder%
+IF not exist %backupFolder% goto FAILPATH_BACK 
+cd %backupFolder%
+md Apps
+cd ..
+XCOPY /S %transmitter%:\Apps %backupFolder%\Apps  
+cd %backupFolder%
+md Audio
+cd ..
+XCOPY /S %transmitter%:\Audio %backupFolder%\Audio 
+cd %backupFolder%
+md Model
+cd ..
+XCOPY /S %transmitter%:\Model %backupFolder%\Model 
+@echo ================================
+@echo partial transmitter backup successful finished
+@echo ================================
+
+:INSTAL
+@echo delete old files on transmitter please wait
+rd %transmitter%:\apps\GW_BATT /S /Q && md %transmitter%:\apps\GW_BATT
+rd %transmitter%:\apps\RCT_BATT /S /Q
+del %transmitter%:\apps\RCT-Batt.*
+del %transmitter%:\apps\lang\RCT-Batt.*
+del %transmitter%:\apps\GW_Batt.*
+del %transmitter%:\apps\lang\GW_Batt.*
+@echo ================================
+@echo copy new files to transmitter please wait
+IF not exist apps\*.* goto FAILPATH_UPD
+setlocal
+%transmitter%:
+cd apps
+md GW_BATT
+endlocal
+XCOPY /S apps\GW_BATT %transmitter%:\apps\GW_BATT
+copy apps\GW_Batt.lc %transmitter%:\apps
+
+@echo ================================
+@echo installation successful finished
+@echo ================================
+goto END
+:FAILLUA
+@echo Installation failed, missing Lua API. Pls. instal Lua API (download Lua update for your transmitter from jetimodel.com )
+goto END
+:FAILPATH
+@echo Installation failed transmitter is not connected or drive letter for transmitter is not correct
+goto END
+:FAILPATH_UPD
+@echo Installation failed the apps folder does not exist.
+goto END
+
+:END
+@PAUSE
+
--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# GW_RCT_Batt_App
-Geierwally's RCT Batt App for Jeti Transmitters (capacity and voltage limit)
-Pls. read pdf files in doku folder before installation and install the App over RCT_Batt_Setup.bat on your transmitter.
+# GW_Batt_App
+Geierwally's battery limit checker app for Jeti 14,16 and 24 - transmitters, checks optional cell voltage limit and capacity limit. 
+With audio messages and separate controls for voltage and capacity limit.
+![Display-picture](https://raw.githubusercontent.com/Geierwally-xD/GW_RCT_Batt_App/master/img/GW_BATT.jpg)
+Pls. read pdf files in doku folder before installation and install the App over GW_Batt_Setup.bat or App Manager on your transmitter.
+On Installation over App Manager you can download the full installation package including user documentation and lua source scripts here:
+https://github.com/Geierwally-xD/GW_RCT_Batt_App
+
+Geierwally's Akku Limit - Checker für Jeti DC/DS 14,16 und 24 Sender. Getrennte Zellspannungs - und Kapazitätsüberwachung.
+Kapazitäts und Spannungslimits werden akustisch ausgegeben. Zusätzlich werden zwei Controls aktiviert, die der Anwender weiter verknüpfen kann.
+Bitte vor Installation Anwenderdoku lesen und über GW_Batt_Setup.bat bzw. App Manager auf dem Sender installieren.
+Bei Installation über App - Manager kann das komplette Installationspacket mit Anwenderdokumentation und Lua - Skripten hier heruntergeladen werden: 
+https://github.com/Geierwally-xD/GW_RCT_Batt_App
